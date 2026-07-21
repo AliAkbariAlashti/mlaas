11466ac2294afe0c009572cc134b5ff8c2aa3fc5
AliAA <ali.akbari.alashti84@gmail.com>
2026-07-21 09:55:17 +0330

    Restore Django admin static assets #17

--- mlaas/urls.py
+++ mlaas/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -13,3 +14,5 @@ urlpatterns = [
     path("api/v1/website/", include("apps.website.urls")),
     path("api/v1/", include("apps.analytics.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
